cd105f8cb2f05c39de4ddde0789fe945b5bfba3f
Barsha <S394996@students.cdu.edu.au>
2026-04-09 20:06:34 +0930

    Files for starting Q2

---
+++ evaluator.py
@@ -0,0 +1,7 @@
+def evaluate_file(input_path: str) -> list[dict]:
+    #implement tokenizer, parser, evaluator
+    return []
+
+if __name__ == "__main__":
+    res = evaluate_file("sample_input.txt")
+    print(res)
